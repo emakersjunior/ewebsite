26f0021c5c6704cf8a94ddd8262cc795691062dd
brunoqs <brun_o13@hotmail.com>
2018-06-09 09:06:10 -0300

    update all

--- ewebsite/wsgi.py
+++ ewebsite/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import time 
+import traceback 
+import signal 
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+#sys.path.append('~/Projetos/ewebsite') 
+# adjust the Python version in the line below as needed 
+#sys.path.append('~/Projetos/.virtualenvs/ewebsite-env/lib/python3.5/site-packages') 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ewebsite.settings")
 
