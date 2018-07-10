3339939ed04c500938c37c6517c555dfe04ce708
franciscone14 <francisconeluiz@gmail.com>
2018-07-10 17:21:02 -0300

    Changed DBMS from Postgres to MySQL

--- ewebsite/settings/development.py
+++ ewebsite/settings/development.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#6rw(sxn+-#b*zup5b8nb(#rsj#@f94hyncbh76x_!(dp$2$9z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.108', 'localhost', '*']
+ALLOWED_HOSTS = ['192.168.0.101', 'localhost', '*']
 
 
 # Application definition
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'ewebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ewebsite',
         # 'NAME': os.path.join(BASE_DIR, 'mydb'),
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '', # 8000 is default
+        'USER': 'emakers',
+        'PASSWORD': 'emakersjr',
+        'HOST': 'localhost',
+        'PORT': '3306', # 8000 is default
     }
 }
 
